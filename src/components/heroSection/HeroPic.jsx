aba21df6469d848bf781e6427508178134b5fcff
Manikanta <mkkinjarapu@gmail.com>
2025-04-06 22:03:54 +0530

    initial commit

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -13,7 +13,7 @@ const HeroPic = () => {
     >
       <img
         src="../../public/images/HexaPic.png"
-        alt="Abdelrahman Qassem"
+        alt="Manikanta Kinjarapu"
         className="max-h-[450px] w-auto"
       />
 
